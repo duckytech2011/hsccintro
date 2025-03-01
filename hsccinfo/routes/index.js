2e1fbc5d87d12385e659991075cd2c7eb656441f
duckytech2011 <malachikenner86@gmail.com>
2025-03-01 17:05:16 +0000

    March update

--- hsccinfo/routes/index.js
+++ hsccinfo/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // DEBUGGING CODE using an if statement and a .env variable
+  if (process.env.CONSOLE_DEBUG=="true"){
+    console.log("Debugging enabled!");
+  }
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+
